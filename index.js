a176f862ef799b26a66a5a7a1293da955b34370e
murodjon00 <erbotayevmurad@gmail.com>
2024-03-14 14:25:28 +0500

    Kill

---
+++ index.js
@@ -0,0 +1,53 @@
+// 1-funksiya linki -> (https://edabit.com/challenge/ARr5tA458o2tC9FTN)
+function hello() {
+    return "hello edabit.com"
+}
+// 2-funksiya linki -> (https://edabit.com/challenge/ARr5tA458o2tC9FTN)
+function addition(a, b) {
+    return a + b
+}
+// 3-funksiya linki -> (https://edabit.com/challenge/8q54MKnRrm89pSLmW)
+function convert(minutes) {
+    return minutes * 60
+}
+
+// 4-funksiya linki -> (https://edabit.com/challenge/ARr5tA458o2tC9FTN)
+
+function addition(num) {
+    return ++num
+}
+// 5-funksiya linki -> (https://edabit.com/challenge/3CaszbdZYGN4otQD8)
+function triArea(base, height) {
+    return base * height / 2
+}
+
+// 6-funksiya linki -> (https://edabit.com/challenge/bL7hSc6Zh4zZJzGmw)
+
+function calcAge(age) {
+	return age * 365
+}
+
+// 7-funksiya linki -> (https://edabit.com/challenge/j7yQbF3J3rToHsDBP)
+
+function cubes(a) {
+	return a ** 3
+}
+
+// 8-funksiya linki -> (https://edabit.com/challenge/QaApgtePE6QrCZ64o)
+
+function getFirstValue(arr) {
+	return arr[0]
+}
+
+// 9-funksiya linki -> (https://edabit.com/challenge/wAdE9te55cowBLcPs)
+
+
+function circuitPower(voltage, current) {
+	return voltage * current
+}
+
+// 10-funksiya linki -> (https://edabit.com/challenge/6AnQqiEjkJdZrWhPS)
+
+function howManySeconds(hours) {
+	return hours * 3600
+}
